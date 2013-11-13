62e0af7227b9e36b74b9813269e01ace843d81a0
Bill Parrott <bill@chimericdream.com>
2013-11-13 08:25:37 -0600

    adding flac package and conf template

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,14 @@
 
 case node[:platform]
 when "ubuntu"
-  %w{abcde lame}.each do |a_package|
+  %w{abcde lame flac}.each do |a_package|
     package a_package
   end
+end
+
+template "/home/vagrant/.abcde.conf" do
+  source "abcde.conf.erb"
+  owner "vagrant"
+  group "vagrant"
+  mode 0644
 end
