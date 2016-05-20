94d63597fa6cb78c8d801cf7f1d144d6d4677227
Bill Parrott <bill@chimericdream.com>
2016-05-20 00:10:23 -0500

    used the wrong format apparently

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name "Chef abcde"
+name "abcde"
 maintainer "Bill Parrott"
 maintainer_email "bill@chimericdream.com"
 license "MIT"
 description "Installs abcde and related dependencies"
-version "0.2.1"
+version "0.2.2"
 recipe "abcde", "Installs abcde"
 
 %w{ ubuntu }.each do |os|
