d73f4186ba974f4ccd6b21019f0a0ea85fa1ccdd
Bill Parrott <bill@chimericdream.com>
2016-05-20 00:01:56 -0500

    add name header and bump patch version

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name "Chef abcde"
 maintainer "Bill Parrott"
 maintainer_email "bill@chimericdream.com"
 license "MIT"
 description "Installs abcde and related dependencies"
-version "0.2.0"
+version "0.2.1"
 recipe "abcde", "Installs abcde"
 
 %w{ ubuntu }.each do |os|
